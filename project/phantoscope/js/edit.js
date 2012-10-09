7925497bf7dd1964f3f9077f5a1ff80f9503a29b
enson <xijian.py@taobao.com>
2012-10-09 19:25:45 +0800

    update

--- project/phantoscope/js/edit.js
+++ project/phantoscope/js/edit.js
@@ -13,11 +13,20 @@ YUI().use('node','node-event-delegate',function(Y){
     var nAddAdButton = Y.one('#add-ad-button');
     var nSave = Y.one('#save-button');
     var nAdListArray = nAdList.all('.ad-item');
+    var nAdListArray2 = nAdListArray.get('length');
     var nAdArrayLength = nAdListArray.get('length').length;
     Y.log(nAdListArray.size());
-    Y.log(nAdListArray.item(0));
-    Y.log(nAdListArray.get('length').length);
-    Y.log(nAdListArray.item(nAdArrayLength-1));
+    //Y.log(nAdListArray.item(0));
+    //Y.log(nAdListArray.item(nAdArrayLength-1));
+    //Y.log(nAdListArray.get('length').length);
+    //Y.log(nAdListArray);
+    //Y.log(nAdListArray.get('length'));
+    //Y.log(nAdListArray2[0]);
+
+    var nBoxLeft = Y.one('#box-left');
+    var nBoxRight = Y.one('#box-right');
+    var nAddTransferBtn = Y.one('#add-transfer-button');
+    var nOutTransferBtn = Y.one('#out-transfer-button');
 
 
     nAddButton.on('click',function(){
@@ -65,21 +74,21 @@ YUI().use('node','node-event-delegate',function(Y){
     function addAd(){
         nAdList.append(
             '<li class="ad-item">'+
-                '<div class="ad-item-pic"><img src="./img/edit/ad_item_pic1.PNG" /></div>'+
-            '<div class="ad-item-button">'+
-                '<img src="./img/edit/check-link.PNG" />'+
-                '<img src="./img/edit/edit.PNG" />'+
-                '<button><img src="./img/edit/delete.PNG" /></button>'+
-            '</div>'+
-            '<div class="ad-item-shift">'+
-                '<div class="item-shift-box">'+
-                    '<div class="item-shift-up2"></div>'+
+                '<div class="ad-item-pic"><img src="./img/edit/phone_ad.PNG" /></div>'+
+                '<div class="ad-item-button">'+
+                    '<img src="./img/edit/check-link.PNG" />'+
+                    '<img src="./img/edit/edit.PNG" />'+
+                    '<button><img src="./img/edit/delete.PNG" /></button>'+
                 '</div>'+
-                '<div class="item-shift-box">'+
-                    '<div class="item-shift-down2"></div>'+
+                '<div class="ad-item-shift">'+
+                    '<div class="item-shift-box">'+
+                        '<div class="item-shift-up2"></div>'+
+                    '</div>'+
+                    '<div class="item-shift-box">'+
+                        '<div class="item-shift-down2"></div>'+
+                    '</div>'+
                 '</div>'+
-            '</div>'+
-        '</li>'
+            '</li>'
         );
     };
     nAddAdButton.on('click',function(){
@@ -91,7 +100,7 @@ YUI().use('node','node-event-delegate',function(Y){
     });
     //判断广告图片数量，继而执行添加广告功能
     function checkAdAmount(){
-        if(nAdArrayLength > 4){
+        if(nAdListArray.size() > 4){
             alert("亲，最多可以添加5张图片哟！");
         }else{
             Y.one('#ad-list').setStyle('display','none');
@@ -112,10 +121,47 @@ YUI().use('node','node-event-delegate',function(Y){
         nAdListArray.item(0)
     }
     */
+    //各广告上下移动
     nAdList.delegate('click',function(){
-        this.ancestor('li').remove();
-
+        for(var i=0; i<nAdListArray.size(); i++){
+            if(this.ancestor('.ad-item') == nAdListArray.item(i)){
+                var prevAdItem = nAdListArray.item(i-1);
+                nAdList.insertBefore( nAdListArray.item(i), prevAdItem);
+                break;
+            }
+        }
+        Y.log(nAdListArray.size());
     },'.item-shift-up2');
+
+    //店铺宝贝框添加移除功能
+    nBoxLeft.delegate('click',function(){
+        this.ancestor('.item-sub').addClass('item-sub-focus');
+    },'.item-sub-dscptn');
+    nBoxLeft.delegate('dblclick',function(){
+        this.ancestor('.item-sub').removeClass('item-sub-focus');
+    },'.item-sub-dscptn');
+    nBoxRight.delegate('click',function(){
+        this.ancestor('.item-sub').removeClass('item-sub-focus');
+    },'.item-sub-dscptn');
+    nBoxRight.delegate('dblclick',function(){
+        this.ancestor('.item-sub').addClass('item-sub-focus');
+    },'.item-sub-dscptn');
+
+    function move(src,des){
+        var nItemListArray = src.all('.item-sub');
+        for(var i=0;i < nItemListArray.size();i++){
+            if(nItemListArray.item(i).hasClass('item-sub-focus')){
+                des.append(nItemListArray.item(i--));
+                break;
+            }
+        }
+    }
+    nAddTransferBtn.on('click',function(){
+        move(nBoxLeft,nBoxRight);
+    });
+    nOutTransferBtn.on('click',function(){
+        move(nBoxRight,nBoxLeft);
+    });
 
 });
 
